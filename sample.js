fa18bd0e1b5776631e3ac34da3ccdacebce35f29
ratishprakash <102710116+ratishprakash@users.noreply.github.com>
2022-04-01 10:52:45 +0530

    Add files via upload

--- sample.js
+++ sample.js
@@ -1,14 +1,33 @@
 var count=0
 function sayhello(){
-    if( chktick.checked){
+    if( chktick.checked)
         count++
-    }
-    else{
+    else 
         count--
-    }
-
-
 }
-function display(){
-    document.write(count)
+function sayhello1(){
+    if(chktick1.checked)
+        count++
+    else
+        count--
+}
+function sayhello2(){
+    if(chktick2.checked)
+        count++
+    else 
+        count--
+}
+function location1(){
+    // var mydiv=document.createElementById("mydiv")
+    // var checkbox = document.createElement('input');
+    // checkbox.type = "checkbox";
+    // checkbox.name = "name";
+    // checkbox.value = "value";
+    // checkbox.id = "id";  
+    // var label=document.createElement("label")
+    // label.htmlFor="id"
+    // label.appendchild(document.createTextNode("Theory Of Computation"))
+    // mydiv.appendChild(checkbox);
+    // mydiv.appendChild(label)
+    alert(count)
 }
