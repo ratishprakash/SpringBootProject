f92c8dad160f8c046d33e93ed077d5a8a8241992
ratishprakash <ratishprakash.p@gmail.com>
2022-04-07 13:29:20 +0530

    some minor changes

--- login_check.js
+++ login_check.js
@@ -1,5 +1,5 @@
-var username = new Array(100)
-var password= new Array(100)
+// var username = new Array(100)
+// var password= new Array(100)
 function check(){
     if(reg.value=="admin" && pass.value=="admin"){
         window.location= "college_library.html"
@@ -18,9 +18,15 @@ function check(){
 function add_student(){
     // var new_username=new_username.value
     // var new_password=new_password.value
-    username.push(new_username.value)
-    password.push(new_password.value)
+    // var a =username.push(new_username.value)
+    // var b =password.push(new_password.value)
+    localStorage.setItem(new_username.value,new_password.value)
     // var a=new_username.value
     alert("document sent")
 
+}
+function display_arr(){
+    for(var i=0;username.length;i++){
+        document.write(username[i]);
+    }
 }
